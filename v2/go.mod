98e960b881ea0badcaa3a1a2a6a5b5db887ff45f
Leo <long.n@kbtg.tech>
2020-01-02 15:33:23 +0700

    v2

---
+++ v2/go.mod
@@ -0,0 +1,5 @@
+module github.com/nthlongtma/go-gomod
+
+go 1.13
+
+require rsc.io/quote/v3 v3.1.0
---
+++ v2/hello.go
@@ -0,0 +1,15 @@
+package hello // import "github.com/nthlongtma/go-gomod/v2"
+
+import "rsc.io/quote/v3"
+
+func Hello() string {
+	return quote.HelloV3()
+}
+
+func Proverb() string {
+	return quote.Concurrency()
+}
+
+func Quote() string {
+	return "versioning is func."
+}
